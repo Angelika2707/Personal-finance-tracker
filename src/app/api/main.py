92a18dc7e8dd4907b0d4362c915dc53b9426743a
Angelika2707 <53828140+Angelika2707@users.noreply.github.com>
2025-04-23 22:15:58 +0300

    delete creation tables without alembic

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -2,8 +2,6 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.database.db_helper import db_helper
-from app.database.models import Base
 from users.views import router as user_router
 from financial_records.views import router as financial_records_router
 import uvicorn
@@ -11,8 +9,6 @@ import uvicorn
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
