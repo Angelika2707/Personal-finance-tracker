a2e231fd009953d17864ee809bf98d1ea67354d3
Angelika2707 <53828140+Angelika2707@users.noreply.github.com>
2025-04-29 14:04:29 +0300

    creation folders for auth and categories

--- src/app/config.py
+++ src/app/config.py
@@ -4,10 +4,22 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent.parent
 
 
-class Settings(BaseSettings):
+class AuthJWT(BaseSettings):
+    private_key_path: Path = BASE_DIR / "certs/jwt-private_key.pem"
+    public_key_path: Path = BASE_DIR / "certs/jwt-public_key.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 3
+
+
+class DbSettings(BaseSettings):
     db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
     db_echo: bool = True
+
+
+class Settings(BaseSettings):
     api_url_records: str = "http://localhost:8000/financial_records/"
+    auth_jwt: AuthJWT = AuthJWT()
+    db: DbSettings = DbSettings()
 
 
 settings = Settings()
