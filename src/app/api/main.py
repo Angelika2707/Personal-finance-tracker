ee5327c0c730bbdffd27aa7ae8e7bc0eaaa4669a
WellNotWell <olesya.gredyushko4956@gmail.com>
2025-05-01 23:19:39 +0300

    add categories router

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 from src.app.api.users.views import router as user_router
 from src.app.api.financial_records.views import router as financial_records_router
+from src.app.api.categories.views import router as categories_router
 import uvicorn
 
 
@@ -12,11 +13,12 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, debug=True)
 
 app.include_router(user_router)
 app.include_router(financial_records_router)
+app.include_router(categories_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, log_level="debug", host="0.0.0.0", port=8000)
